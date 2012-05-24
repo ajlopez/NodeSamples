90149a021f89882dffebcf0394237fcb3e98c495
ajlopez <webmaster@ajlopez.com>
2012-05-24 06:48:51 -0300

    Removing old ExpressSite, now in Express/Site

--- ExpressSite/hello.js
+++
@@ -1,5 +0,0 @@
-var app = require('express').createServer();
-app.get('/', function(req, res) {
-res.send('Hello, world!');
-});
-app.listen(3000);
--- ExpressSite/hello2.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express.createServer(
-    express.logger()
-);
-app.get('/', function(req, res) {
-res.send('Hello, world!');
-});
-app.listen(3000);

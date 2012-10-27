7bfa7ce5c7590eb4a02fa5f1e6a63bcaeba910a7
ajlopez <webmaster@ajlopez.com>
2012-10-27 11:55:12 -0300

    Express Blog new page processing

--- Express/Blog/app.js
+++ Express/Blog/app.js
@@ -43,6 +43,21 @@ app.get('/', function(req, res){
     })
 });
 
+app.get('/blog/new', function(req, res) {
+    res.render('blog_new.jade', { 
+        title: 'New Post'
+    });
+});
+
+app.post('/blog/new', function(req, res){
+    articleProvider.save({
+        title: req.param('title'),
+        body: req.param('body')
+    }, function( error, docs) {
+        res.redirect('/')
+    });
+});
+
 app.get('/blog/:id', function(req, res) {
     articleProvider.findById(req.params.id, function(error, article) {
         res.render('blog_show.jade',

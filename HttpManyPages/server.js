5dd183075ba20c90c71382239f26453ed7ee64ee
ajlopez <webmaster@ajlopez.com>
2012-10-30 14:21:24 -0300

    Update pages

--- HttpManyPages/server.js
+++ HttpManyPages/server.js
@@ -1,11 +1,20 @@
 
 var http = require('http');
 
+// a.name = "Adam";
+// a['name'] = "Adam";
+
+
+var mapping = {
+    '/index.html': '<h1>Hello Index<h1>',
+    '/cart.html': '<h1>Shoppint cart</h1>',
+    '/about.html': '<h1>About</h1>'
+};
+
 var server = http.createServer(function(req, res) {
     res.writeHeader(200, {'Content-type': 'text/html'});
-    res.write("<h1>");
-    res.write(req.url);
-    res.end("</h1>");
+    
+    res.end(mapping[req.url]);
 }
 );
 

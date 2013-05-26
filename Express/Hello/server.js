e2762d42a011c8f301de349fa82ca72b4ce67b03
ajlopez <webmaster@ajlopez.com>
2013-05-26 14:56:06 -0300

    Adapted to Heroku

--- Express/Hello/server.js
+++ Express/Hello/server.js
@@ -1,13 +1,15 @@
-
-var express = require('express');
-
+var express = require("express");
 var app = express();
+app.use(express.logger());
 
-app.get('/', function(req, res) {
-	res.send('Hello, world');
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-var server = app.listen();
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 
-console.log('Express server started on port %s', server.address().port);
+
 

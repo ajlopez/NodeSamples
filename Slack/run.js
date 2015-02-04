33fc62f283b6b79e2d7769e7b97ac88fb07861bc
ajlopez <webmaster@ajlopez.com>
2015-02-04 15:53:31 -0300

    Using web hook

--- Slack/run.js
+++ Slack/run.js
@@ -1,10 +1,12 @@
 
-var domain = process.env.SLACK_DOMAIN;
-var token = process.env.SLACK_TOKEN;
+//var domain = process.env.SLACK_DOMAIN;
+var hookUrl = process.env.SLACK_WEBHOOK;
+//var token = process.env.SLACK_TOKEN;
 var username = process.env.SLACK_USER;
 
 var Slack = require('node-slack');
-var slack = new Slack(domain, token);
+//var slack = new Slack(domain, token);
+var slack = new Slack(hookUrl);
 
 var channel = process.argv[2];
 var message = process.argv[3];
